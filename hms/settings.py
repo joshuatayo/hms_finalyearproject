00ca184a65b9be3e12cfb59d019591b210d4e75e
joshphur <j4matt@gmail.com>
2021-05-08 12:46:57 +0100

    second commit

--- hms/settings.py
+++ hms/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'joshuatayomatthew@gmail.com'
-EMAIL_HOST_PASSWORD = '080matthew'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
